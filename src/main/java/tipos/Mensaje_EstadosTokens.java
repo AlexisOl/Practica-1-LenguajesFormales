ec76aeef1a0f73b1aceceb92f0440bc54be7624b
AlexisOl <85314303+AlexisOl@users.noreply.github.com>
2021-08-06 00:28:30 -0600

    Add files via upload

---
+++ src/main/java/tipos/Mensaje_EstadosTokens.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tipos;
+
+/**
+ *
+ * @author alexis
+ */
+public enum Mensaje_EstadosTokens {
+    // Tipos de estados existentes
+    
+    
+EstadoEspacio, Estadodigito, Estadoid,
+EstadoSimbolo, EstadoDecimal, EstadoError,
+EstadoClaseNumero;
+           
+}
---
+++ src/main/java/tipos/Simbolo.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tipos;
+
+/**
+ *
+ * @author alexis
+ */
+public class Simbolo {
+        // metodo para ver si hay un simbolo 
+    
+   public static boolean simbolo(char actual){
+        if ((actual=='{')||(actual =='}')||(actual=='[')||(actual ==']') || (actual==';')||(actual==',')){
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
